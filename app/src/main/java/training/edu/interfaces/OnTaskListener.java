6d524f6fdb9c905bc5d6b2a5221c7355450a0efb
darkgeat <darkgeat@gmail.com>
2017-08-20 14:17:49 -0500

    Creation of interfaces

---
+++ app/src/main/java/training/edu/interfaces/OnTaskListener.java
@@ -0,0 +1,11 @@
+package training.edu.interfaces;
+
+/**
+ * @author Giovani González
+ * Created by darkgeat on 8/20/17.
+ */
+
+public interface OnTaskListener {
+    void OnTaskCompleted(String response);
+    void OnTaskError(int errorCode, String message);
+}
